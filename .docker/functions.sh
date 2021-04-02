1de43c5cdb929fc2360a659337ae01345fd45735
Tudor Rusu <tudor.rusu@hotmail.com>
2021-04-02 15:35:52 +0200

    refactor(app): adjust MCrypt PHP extension install

--- .docker/functions.sh
+++ .docker/functions.sh
@@ -144,6 +144,7 @@ function removeAllApp() {
 function removeLaravel() {
     sed -i '/mcryptSupport/d' .docker/build/php/Dockerfile
     sed -i '/mcryptInstall/d' .docker/build/php/Dockerfile
+    sed -i '/mcryptEnable/d' .docker/build/php/Dockerfile
     sed -i '/bcmathInstall/d' .docker/build/php/Dockerfile
 
     replaceAllInFile .docker/build/nginx/conf.d/app.conf "rootDefinition" "root   /var/www;";
@@ -166,7 +167,8 @@ function removeLaravel() {
 function addMCryptExt() {
     echo "Add MCrypt extension"
     replaceAllInFile .docker/build/php/Dockerfile "mcryptSupport" "libmcrypt-dev";
-    replaceAllInFile .docker/build/php/Dockerfile "mcryptInstall" "RUN docker-php-ext-install mcrypt";
+    replaceAllInFile .docker/build/php/Dockerfile "mcryptInstall" "pecl install mcrypt-1.0.4";
+    replaceAllInFile .docker/build/php/Dockerfile "mcryptEnable" "RUN docker-php-ext-enable mcrypt";
 }
 
 function addBCMathExt() {
